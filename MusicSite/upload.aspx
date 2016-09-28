a807ced2ddb810854c991155c41d3e9ad6751310
privacyismyth <privacyismyth@gmail.com>
2016-09-28 21:41:13 +0530

    project initialised

---
+++ MusicSite/upload.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="upload.aspx.cs" MasterPageFile="~/Master.Master"
+    Inherits="MusicSite.index" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="head">
+    <link rel="stylesheet" type="text/css" href="Style/bootstrap.css" />
+</asp:Content>
+<asp:Content runat="server" ContentPlaceHolderID="ContentPlaceHolder1">
+    <div class="container-fluid">
+        <asp:FileUpload ID="FileUpload1" runat="server"  />
+        <br />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="UploadFile" CssClass="btn-success" />
+        <hr />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover table-bordered"
+            AutoGenerateColumns="false" EmptyDataText="No files uploaded">
+            <Columns>
+                <asp:BoundField DataField="Text" HeaderText="File Name" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkDownload" Text="Download" CommandArgument='<%# Eval("Value") %>'
+                            runat="server" OnClick="DownloadFile"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkDelete" Text="Delete" CommandArgument='<%# Eval("Value") %>'
+                            runat="server" OnClick="DeleteFile" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+</asp:Content>
---
+++ MusicSite/upload.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace MusicSite
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                string[] filePaths = Directory.GetFiles(Server.MapPath("~/Data/"));
+                List<ListItem> files = new List<ListItem>();
+                foreach (string filePath in filePaths)
+                {
+                    files.Add(new ListItem(Path.GetFileName(filePath), filePath));
+                }
+                GridView1.DataSource = files;
+                GridView1.DataBind();
+            }
+
+        }
+        protected void UploadFile(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (FileUpload1.HasFile)
+                {
+                    string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Data/") + fileName);
+                    Response.Redirect(Request.Url.AbsoluteUri);
+
+                }
+                else
+                {
+                    Label1.Text = "Choose a File to Upload";
+                }
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = "Error Occured";
+            }
+
+
+        }
+        protected void DownloadFile(object sender, EventArgs e)
+        {
+            string filePath = (sender as LinkButton).CommandArgument;
+            Response.ContentType = ContentType;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(filePath));
+            Response.WriteFile(filePath);
+            Response.End();
+        }
+        protected void DeleteFile(object sender, EventArgs e)
+        {
+            string filePath = (sender as LinkButton).CommandArgument;
+            File.Delete(filePath);
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+
+
+
+    }
+}
